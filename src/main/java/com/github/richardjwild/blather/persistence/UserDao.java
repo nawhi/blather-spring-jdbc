57a7e2d317be2903e73eaa9764cc2693ef47e9ad
Bartomeu <bartomeugm@gmail.com>
2018-12-03 15:53:17 +0100

    UserDao implemented

--- src/main/java/com/github/richardjwild/blather/persistence/UserDao.java
+++ src/main/java/com/github/richardjwild/blather/persistence/UserDao.java
@@ -9,27 +9,50 @@ public class UserDao {
         Connection conn = null;
         PreparedStatement statement ;
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/blather" +
-                    "?user=root&password=password");
+            conn = createConnection();
             statement = conn.prepareStatement("INSERT INTO users VALUES (?)");
             statement.setString(1, name);
             statement.execute();
         } catch (SQLException e) {
             System.err.println("Connection failed: "+ e.getMessage());
         } finally {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            closeConnection(conn);
+        }
+    }
+
+    private Connection createConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/blather" +
+                "?user=root&password=password");
+    }
+
+    private void closeConnection(Connection conn) {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        new UserDao().saveUser("testuser");
+       User user = new UserDao().findUser("testuser");
+       System.out.println("Username: " + user.name());
     }
 
     public User findUser(String name) {
-      return null;
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet results = null;
+        try {
+            conn = createConnection();
+            statement = conn.prepareStatement("SELECT * FROM users WHERE name = ?");
+            statement.setString(1, name);
+            results = statement.executeQuery();
+            if(results.next()) return new User(results.getString(1));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection(conn);
+        }
+        return null;
     }
 }

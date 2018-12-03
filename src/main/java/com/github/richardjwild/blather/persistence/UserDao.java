939deb8e7b68b89b084ec30d6ac34c8f296713fa
Bartomeu <bartomeugm@gmail.com>
2018-12-03 15:15:38 +0100

    UserDao save implemented

--- src/main/java/com/github/richardjwild/blather/persistence/UserDao.java
+++ src/main/java/com/github/richardjwild/blather/persistence/UserDao.java
@@ -2,9 +2,31 @@ package com.github.richardjwild.blather.persistence;
 
 import com.github.richardjwild.blather.user.User;
 
+import java.sql.*;
+
 public class UserDao {
     public void saveUser(String name) {
+        Connection conn = null;
+        PreparedStatement statement ;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/blather" +
+                    "?user=root&password=password");
+            statement = conn.prepareStatement("INSERT INTO users VALUES (?)");
+            statement.setString(1, name);
+            statement.execute();
+        } catch (SQLException e) {
+            System.err.println("Connection failed: "+ e.getMessage());
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        new UserDao().saveUser("testuser");
     }
 
     public User findUser(String name) {

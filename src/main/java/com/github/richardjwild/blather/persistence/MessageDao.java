39d89d93a1904a6c544a49007bb39cfeb2282420
Bartomeu <bartomeugm@gmail.com>
2018-12-04 15:35:36 +0100

    Retrieved messages from DB

--- src/main/java/com/github/richardjwild/blather/persistence/MessageDao.java
+++ src/main/java/com/github/richardjwild/blather/persistence/MessageDao.java
@@ -1,15 +1,35 @@
 package com.github.richardjwild.blather.persistence;
 
+
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MessageDao {
-    public List<MessageDto> getMessagesFor(String userName) {
-        return null;
+    private final Connection connection;
+
+    public MessageDao(Connection connection) {
+
+        this.connection = connection;
     }
 
-    public static void main(String[] args) {
-        MessageDao messageDao = new MessageDao();
-        List<MessageDto> messages = messageDao.getMessagesFor("testuser");
-        messages.forEach(System.out::println);
+    public List<MessageDto> getMessagesFor(String userName) {
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        List<MessageDto> messages = new ArrayList<>();
+        try {
+            statement = connection.prepareStatement("SELECT recipient, text, post_time FROM messages WHERE recipient = ?");
+            statement.setString(1, userName);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String recipient = resultSet.getString(1);
+                String text = resultSet.getString(2);
+                Timestamp timestamp = resultSet.getTimestamp(3);
+                messages.add(new MessageDto(recipient, text, timestamp));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return messages;
     }
 }

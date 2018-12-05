0c928cb651a758591cfe61b7ad9564188716593a
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-05 13:45:15 +0000

    Remove import

--- src/main/java/com/github/richardjwild/blather/Blather.java
+++ src/main/java/com/github/richardjwild/blather/Blather.java
@@ -9,7 +9,6 @@ import com.github.richardjwild.blather.time.SystemClock;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Blather {

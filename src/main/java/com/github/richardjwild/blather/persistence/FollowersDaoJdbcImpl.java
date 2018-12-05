6fbf9e06bbb4984f9cb0293be0452a72029c4c6e
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-05 16:25:55 +0000

    Save followees from JDBC implementation

--- src/main/java/com/github/richardjwild/blather/persistence/FollowersDaoJdbcImpl.java
+++ src/main/java/com/github/richardjwild/blather/persistence/FollowersDaoJdbcImpl.java
@@ -2,11 +2,15 @@ package com.github.richardjwild.blather.persistence;
 
 import com.github.richardjwild.blather.user.User;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FollowersDaoJdbcImpl implements FollowersDao {
     private JdbcTemplate jdbcTemplate;
@@ -18,7 +22,26 @@ public class FollowersDaoJdbcImpl implements FollowersDao {
 
     @Override
     public void saveFollowees(String follower, Set<User> followees) {
+        Set<String> currentFollowees = getFollowees(follower);
+        List<User> followeesToBeSaved = followees.stream()
+                .filter(f -> !currentFollowees.contains(f))
+                .collect(Collectors.toList());
 
+        final String sql = "INSERT INTO followers VALUES (?, ?)";
+        jdbcTemplate.batchUpdate(
+                sql,
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setString(1, follower);
+                        ps.setString(2, followeesToBeSaved.get(i).name());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return followeesToBeSaved.size();
+                    }
+        });
     }
 
     @Override
@@ -39,7 +62,11 @@ public class FollowersDaoJdbcImpl implements FollowersDao {
         dataSource.setPort(3306);
         
         var dao = new FollowersDaoJdbcImpl(new JdbcTemplate(dataSource));
+
+        dao.saveFollowees("toni", Set.of(new User("alice"), new User("bob")));
+
         dao.getFollowees("toni").stream().forEach(System.out::println);
+        // nick, alice, bob
         
     }
 }

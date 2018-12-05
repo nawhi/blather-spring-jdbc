3926de4d183c63059fabb9b9b9e380d9f9ee4314
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-05 11:53:48 +0000

    Add Spring JDBC dependencies

--- src/main/java/com/github/richardjwild/blather/persistence/UserDao.java
+++ src/main/java/com/github/richardjwild/blather/persistence/UserDao.java
@@ -1,40 +1,4 @@
 package com.github.richardjwild.blather.persistence;
 
-import com.github.richardjwild.blather.user.User;
-
-import java.sql.*;
-
-public class UserDao {
-    private Connection connection;
-
-    public UserDao(Connection connection) {
-        this.connection = connection;
-    }
-
-    public void saveUser(String name) {
-        PreparedStatement statement ;
-        try {
-            statement = connection.prepareStatement("INSERT INTO users VALUES (?)");
-            statement.setString(1, name);
-            statement.execute();
-        } catch (SQLException e) {
-            System.err.println("Saveuser failed: "+ e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    public String findUser(String name) {
-        PreparedStatement statement = null;
-        ResultSet results = null;
-        try {
-            statement = connection.prepareStatement("SELECT * FROM users WHERE name = ?");
-            statement.setString(1, name);
-            results = statement.executeQuery();
-            if(results.next()) return results.getString(1);
-        } catch (SQLException e) {
-            System.err.println("Finduser failed: "+ e.getMessage());
-            e.printStackTrace();
-        }
-        return null;
-    }
+public interface UserDao {
 }

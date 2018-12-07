85469bfd6801ca8c8bb4e280d30863dc0269c8bc
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-07 14:04:18 +0000

    Implement UserDao

--- src/main/java/com/github/richardjwild/blather/persistence/UserDaoMongoImpl.java
+++ src/main/java/com/github/richardjwild/blather/persistence/UserDaoMongoImpl.java
@@ -1,19 +1,33 @@
 package com.github.richardjwild.blather.persistence;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
+import org.bson.Document;
 
 public class UserDaoMongoImpl implements UserDao {
-    public UserDaoMongoImpl(MongoClient client) {
+    private MongoClient client;
+    private final MongoDatabase database;
+    private final MongoCollection<Document> collection;
 
+    public UserDaoMongoImpl(MongoClient client) {
+        this.client = client;
+        database = client.getDatabase("blather");
+        collection = database.getCollection("collection");
     }
 
     @Override
     public void saveUser(String name) {
-
+        collection.insertOne(new Document("name", name));
     }
 
     @Override
     public String findUser(String name) {
-        return null;
+        BsonDocument filter = new BsonDocument("name", new BsonString(name));
+        Document firstResult = collection.find(filter).first();
+        return firstResult.getString("name");
     }
 }
